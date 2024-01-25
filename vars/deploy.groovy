887a8107bf0aca1f0bf3ed122b2bcbbb1b845ee8
Marwan A El-sharkawy <67644687+Marwan465@users.noreply.github.com>
2024-01-25 23:37:04 +0200

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -6,7 +6,7 @@ def openshiftDeploy(String OPENSHIFT_CONFIG_FILE, String nameSpace, String yamlP
                     withCredentials([file(credentialsId: OPENSHIFT_CONFIG_FILE, variable: 'KUBE_CONFIG_FILE')]) {
            env.KUBECONFIG = sh(script: "echo \$KUBE_CONFIG_FILE", returnStdout: true).trim() 
                     sh "chmod 600 \$KUBECONFIG"
-                    sh "oc project \${nameSpace} --kubeconfig=\$KUBECONFIG"
+                    sh "oc project ${nameSpace} --kubeconfig=\$KUBECONFIG"
                     sh "oc apply -f ${yamlPath} --kubeconfig=\$KUBECONFIG"
                     
                     }
@@ -17,7 +17,7 @@ def asService(String OPENSHIFT_CONFIG_FILE, String deploymentName) {
         script {
              withCredentials([file(credentialsId: OPENSHIFT_CONFIG_FILE, variable: 'KUBE_CONFIG_FILE')]) {
            env.KUBECONFIG = sh(script: "echo \$KUBE_CONFIG_FILE", returnStdout: true).trim()
-            sh "oc expose deployment \${deploymentName} --kubeconfig=\$KUBECONFIG"
+            sh "oc expose deployment ${deploymentName} --kubeconfig=\$KUBECONFIG"
   
         }
         }
@@ -30,7 +30,7 @@ def createRoute(String OPENSHIFT_CONFIG_FILE, String serviceName) {
            env.KUBECONFIG = sh(script: "echo \$KUBE_CONFIG_FILE", returnStdout: true).trim()
             
             
-            sh "oc create route edge --service=\${serviceName}"
+            sh "oc create route edge --service=${serviceName}"
         }
     }
 }
