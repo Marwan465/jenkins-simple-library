760bed14e75658a950c5902e570bbb3ab6298ef1
Marwan A El-sharkawy <67644687+Marwan465@users.noreply.github.com>
2024-01-25 21:49:15 +0200

    Update dockerize.groovy

--- vars/dockerize.groovy
+++ vars/dockerize.groovy
@@ -13,7 +13,7 @@ def buildDockerImage(String DockerRegistry, String image){
 
 }
 
-def pushDockerImage(String registryCredential, Image dockerImage){
+def pushDockerImage(String registryCredential, String dockerImage){
     script{
         def commitHash = getCommitHash()
         withCredentials([usernamePassword(credentialsId: registryCredential, usernameVariable: 'DOCKER_HUB_USERNAME', passwordVariable: 'DOCKER_HUB_PASSWORD')]){
